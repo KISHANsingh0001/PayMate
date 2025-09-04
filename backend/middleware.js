2b7cd292d89498b7d85193ba27f8e9ed5e770703
Kishan <kishansinghthakur27@gmail.com>
2025-09-04 16:03:22 +0530

    Refactor auth middleware to use JWT_SECRET from config and update BACKEND_URL in config.js

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,3 +1,5 @@
+const { JWT_SECRET } = require("./config");
+const jwt = require("jsonwebtoken");
 
 const authMiddleWare = (req,res,next) => {
     const authHeader = req.headers.authorization;
@@ -9,7 +11,7 @@ const authMiddleWare = (req,res,next) => {
     const token = authHeader.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, JWT_SECRET);
 
         req.userId = decoded.userId;
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
 export  const BACKEND_URL = "https://paymate-production.up.railway.app";
+// export  const BACKEND_URL = "https://pay-mate-qo3v.vercel.app";
